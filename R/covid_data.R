ffb46e40e0c2c65d4b8901ed68790606eb59c931
Gordon Grabert <gordon.grabert@tu-braunschweig.de>
2023-11-10 16:14:20 +0100

    update website and added covid data

--- R/covid_data.R
+++ R/covid_data.R
@@ -1,6 +1,8 @@
-#' Example data to test MeTEor
+#' Real world metabolomics data set to test MeTEor
 #'
-#' An example longitudinal data set to test MeTEor
+#' An example real world metabolomics data set from the study "Plasma Metabolome Alterations Discriminate between COVID-19 and Non-COVID-19 Pneumonia" to test MeTEor.
+#'
+#' @references More, T. H., Mozafari, B., Märtens, A., Herr, C., Lepper, P. M., Danziger, G., ... & Bals, R. (2022). Plasma Metabolome Alterations Discriminate between COVID-19 and Non-COVID-19 Pneumonia. Metabolites, 12(11), 1058.
 #'
 #'
 #' @docType data
--- data-raw/covid_data.R
+++ data-raw/covid_data.R
@@ -1,3 +1,3 @@
 ## code to prepare `example_data` dataset goes here
-covid_data <- readr::read_csv("covid_data.csv")
+covid_data <- readr::read_csv("inst/my_app/covid_data.csv")
 usethis::use_data(covid_data, overwrite = TRUE)
